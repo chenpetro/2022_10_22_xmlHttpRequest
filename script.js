3c7646c1447b71b19515971b0ae4ad91b1796247
chenpetro <chenpetro@gmail.com>
2025-04-20 19:38:13 +0200

    xmlhttp request example . Get

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
-function getPosts(callback){
-    
-    const xhr = new XMLHttpRequest();
-    console.log(xhr);
-    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
-    xhr.addEventListener('load', () =>{
-        console.log('Loaded successfully');
-        const response = JSON.parse(xhr.response)
-        console.log(response);
+const btnGetPosts = document.querySelector(".get-posts");
 
-        callback(response)
-    })
-    
-    xhr.addEventListener('error', () =>{
-        console.log('not OK');
-    
-    })
-    
-    xhr.send()
+function getPosts(callback) {
+  const xhr = new XMLHttpRequest();
+  console.log(xhr);
+  xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+  xhr.addEventListener("load", () => {
+    console.log("Loaded successfully");
+    const response = JSON.parse(xhr.response);
+    console.log(response);
 
+    callback(response);
+  });
 
+  xhr.addEventListener("error", () => {
+    console.log("not OK");
+  });
+
+  xhr.send();
 }
 
-
-getPosts((response) =>{
+btnGetPosts.addEventListener("click", () => {
+  getPosts((response) => {
     console.log(response);
-    
+  });
 });
-
-
-
