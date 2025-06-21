bc4a4e5d8d1d357ff6a498c5b2e407c01fc81944
chenpetro <chenpetro@gmail.com>
2025-06-21 14:10:54 +0200

    Posts are created from JSON and displayed o a click

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const btnGetPosts = document.querySelector(".get-posts");
+const postBox = document.querySelector(".posts-box");
+
+
+
 
 function getPosts(callback) {
   const xhr = new XMLHttpRequest();
-  console.log(xhr);
+  // console.log(xhr);
   xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
   xhr.addEventListener("load", () => {
     console.log("Loaded successfully");
@@ -21,6 +25,63 @@ function getPosts(callback) {
 
 btnGetPosts.addEventListener("click", () => {
   getPosts((response) => {
-    console.log(response);
+    // console.log(response);
+    
+    const fragment = document.createDocumentFragment()
+    response.forEach(post => {
+    const card = document.createElement("div");
+    // card.className = "card";
+    card.classList.add('card');
+    const cardBody = document.createElement("div");
+    cardBody.classList.add('card-body');
+    const title = document.createElement('h4');
+    title.classList.add('card__title');
+    title.textContent = post.title; 
+    const article = document.createElement('p');
+    const id = document.createElement('div');
+    id.classList.add('card__id');
+    id.textContent = post.id;
+    article.classList.add('card__text');
+    article.textContent = post.body; 
+    cardBody.append(title, article, id);
+    card.appendChild(cardBody);
+    
+
+
+
+    
+      // innerHTML
+      // textContent
+      // innerText
+      // nodeValue
+
+
+    console.log(card);
+    fragment.appendChild(card);
+
+    })
+    postBox.appendChild(fragment);
   });
 });
+
+////////////////////////////////////////////
+
+
+// function hi() {
+//   let a = '1234';
+//   result(a)
+// }
+
+// hi(a)
+
+// console.log(a);
+
+
+//////////////////////////////////////////////
+
+// let user = {
+//   name: 'ivan',
+//   age: 22
+// }
+
+// console.log(user.name);
